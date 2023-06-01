1c04476ec19f18bc4eb4dd5b5c52686eb2273217
Seascouter <seascouterdev@icloud.com>
2023-06-01 10:00:38 -0400

    Added structures.py

---
+++ structures.py
@@ -0,0 +1,8 @@
+class Set:
+    def __init__(self, contents):
+        self.contents = contents
+
+class Card:
+    def __init(self, side1, side2):
+        self.side1 = side1
+        self.side2 = side2
