b7fef2aeb118d3416d2f1945cebd89f60d19b710
Seascouter <seascouterdev@icloud.com>
2023-06-21 21:14:59 -0400

    Modified files for mass commit

--- quizi.py
+++ quizi.py
@@ -1,3 +1,24 @@
+import structures
 
+currentSet = structures.Set("",[])
 
+def loadSet(setName):
+    global currentSet
+    newSet = structures.Set("",[])
+    with open(f"sets/{setName}.set") as f:
+        for line in f:
+            if line[0] != "{":
+                newSet.name = line.strip()
+                continue
+            line = line.strip()
+            line = line[1:-1]
+            line = line.split(":")
+            for item in range(len(line)):
+                line[item] = line[item][1:-1]
+            newCard = structures.Card(line[0], line[1])
+            newSet.contents.append(newCard)
+    currentSet = newSet
 
+def unloadSet():
+    global currentSet
+    currentSet = structures.Set("",[])
--- structures.py
+++ structures.py
@@ -1,8 +1,9 @@
 class Set:
-    def __init__(self, contents):
+    def __init__(self, name, contents):
+        self.name = name
         self.contents = contents
 
 class Card:
-    def __init(self, side1, side2):
+    def __init__(self, side1, side2):
         self.side1 = side1
         self.side2 = side2
